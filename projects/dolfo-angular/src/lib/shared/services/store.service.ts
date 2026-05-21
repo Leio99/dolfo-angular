a27dc71e1e36c6e97b1950e2b2f9aefd9844bf7f
Leio99 <amicoexe@gmail.com>
2026-05-21 22:01:52 +0200

    Fix store service

--- projects/dolfo-angular/src/lib/shared/services/store.service.ts
+++ projects/dolfo-angular/src/lib/shared/services/store.service.ts
@@ -1,12 +1,33 @@
 import { Injectable, signal } from "@angular/core"
-import { BehaviorSubject } from "rxjs"
+import { BehaviorSubject, filter, fromEvent, tap, zip } from "rxjs"
+// ! Importante che venga importato con percorso preciso
+import { Subscriptable } from "../classes/subscriptable"
 import { IStore, StorageListType, StoreLists } from "../interfaces"
 
 @Injectable({
     providedIn: "root"
 })
-export class StoreService {
+export class StoreService extends Subscriptable {
     private appInitialized = signal(false)
+
+    constructor(){
+        super()
+
+        this.addSubscription(zip([
+            fromEvent<KeyboardEvent>(window, "keydown").pipe(
+                filter(e => !this.isPressingCtrl() && e.ctrlKey),
+                tap(() => this.setCtrlKey(true))
+            ),
+            fromEvent<KeyboardEvent>(window, "keyup").pipe(
+                filter(e => this.isPressingCtrl() && e.key === "Control"),
+                tap(() => this.setCtrlKey(false))
+            ),
+            fromEvent<KeyboardEvent>(window, "blur").pipe(
+                filter(() => this.isPressingCtrl()),
+                tap(() => this.setCtrlKey(false))
+            )
+        ]).subscribe())
+    }
 
     private store$ = new BehaviorSubject<IStore>({
         lists: {},
